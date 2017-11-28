5141bb8927e849bcd9d4e9515d20aaa30867fe96
Javier Riera Peiro <jariepei@gmail.com>
2017-11-28 14:33:18 +0000

    Updated config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ec2-user',
-      host: 'ec2-54-194-45-162.eu-west-1.compute.amazonaws.com',
+      host: 'ec2-54-246-138-122.eu-west-1.compute.amazonaws.com',
       key: '~/certs/testing-box.pem',
       ref: 'origin/master',
       repo: 'https://github.com/jrierapeiro/node_web_app.git',
