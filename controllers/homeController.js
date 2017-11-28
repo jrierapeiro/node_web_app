b807ca54ab82aae0a67f281470db919f7671bb35
jrierapeiro <jrierapeiro@ancestry.com>
2017-11-28 16:26:09 +0000

    Added version at index view

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,8 @@
 'use strict';
+const config = require('config');
 
 const getIndexPage = (req, res, next) => {
-  return res.render('index');
+  return res.render('index', { model: config.version });
 };
 
 module.exports = { getIndexPage };
